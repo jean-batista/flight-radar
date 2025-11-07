b0d4a39f275a8410bc13837ea1dd6464efbe6edf
Jean Batista <jeanbatista0503@gmail.com>
2025-11-07 14:41:37 -0300

    feat(frontend): implementa a tela de login
    Cria a estrutura JSX e o CSS para a nova página de Login.
    Aproveita para aplicar a fonte 'Roboto' globalmente no App.css.

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,64 @@
 // CSS
 import "./Login.css";
 
+// Hooks
+import { useState } from "react";
+
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(email === "") {
+      setError("Preencha o campo de email!");
+      return;
+    }
+
+
+    if(password === "") {
+      setError("A senha não pode estar vazia!");
+      return;
+    }
+
+    if(email !== "test@test.com") {
+      setError("Email incorreto!");
+      return;
+    }
+
+    if(password !== "admin") {
+      setError("Senha incorreta!");
+      return;
+    }
+
+    setError(null);
+
+    window.alert("Login realizado com sucesso!");
+  }
+
   return (
-    <div>
-        <h1>Login Page</h1>
-    </div>
+    <main className="login-container">
+      <h1>Bem-vindo ao MSN</h1>
+      <p className="hint">Acesse sua conta com e-mail e senha.</p>
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        {error && <p className="form-error">{error}</p>}
+        <div className="actions">
+            <span>Não tem uma conta?<a href="#">Registre-se</a></span>
+        </div>
+        <input type="submit" value="Login" className="btn" />
+      </form>
+    </main>
   )
 }
 
